22ab5b78c2f43dfe47aa2be74ed48e8e90107f84
Robert Fiser <robert.fiser@coinmate.io>
2025-12-17 02:23:29 +0100

    Refactor `HrvControl.java` to streamline item change processing, optimize state handling, and remove redundant logging.

--- openhab-dev/conf/automation/jsr223/HrvControl.java
+++ openhab-dev/conf/automation/jsr223/HrvControl.java
@@ -17,25 +17,21 @@ public class HrvControl extends Java223Script {
   @Rule(name = "item.changed", description = "Handle item changes")
   @ItemStateChangeTrigger(itemName = "*")
   public void onZigbeeItemChanged() {
-    logger.info("Item changed: {}", items);
-    HabStateFactory.writeState(_items.gOutputs(), events, getState().withTemporaryManualMode(true));
-  }
-
-  @Rule(name = "manual.mode", description = "Handle item changes")
-  @ItemStateChangeTrigger(itemName = HabState.Fields.manualPower)
-  public void onManualPowerChanged() {
-    logger.info("Manual mode changed to {}", getState().manualMode());
-    events.sendCommand(_items.temporaryManualMode(), OnOffType.ON);
-  }
-
-  private HabState getState() {
     MqttItemMappings itemMappings =
         MqttItemMappings.builder()
             .of(Fields.smoke, _items.mqttZigbeeSmoke_0xa4c138aa8b540e22())
             .of(Fields.humidity, _items.mqttZigbeeHumidity_0x00158d008b8b7beb())
             .build();
 
-    HabState inputState = HabStateFactory.of(items, itemMappings);
-    return new HrvCalculator().calculate(inputState);
+    var inputState = HabStateFactory.of(items, itemMappings);
+    inputState = new HrvCalculator().calculate(inputState);
+
+    HabStateFactory.writeState(_items.gOutputs(), events, inputState);
+  }
+
+  @Rule(name = "manual.mode", description = "Handle item changes")
+  @ItemStateChangeTrigger(itemName = HabState.Fields.manualPower)
+  public void onManualPowerChanged() {
+    events.sendCommand(_items.temporaryManualMode(), OnOffType.ON);
   }
 }
